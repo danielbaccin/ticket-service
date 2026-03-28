a15b06cbf2b2e4bb822c671fe84168177ef84aed
Daniel Baccin <daniel.baccin@gmail.com>
2026-03-28 20:58:32 -0300

    fix: servers

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,18 @@
 import Fastify from 'fastify'
 import { orderRoutes } from './modules/orders/orders.controller'
+import { pool } from './db/pool'
 
 const app = Fastify({ logger: true })
 
+// rotas
 app.register(orderRoutes, { prefix: '/orders' })
 
+app.get('/health', async () => {
+  const result = await pool.query('SELECT 1')
+  return { ok: true, db: result.rows }
+})
+
+// subir servidor (SEMPRE por último)
 app.listen({ port: 3000 }).then(() => {
   console.log('🚀 Server rodando na porta 3000')
 })
