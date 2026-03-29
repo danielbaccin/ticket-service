cec3f5652036aa8cc85db98d8381c61e35c1e95d
Daniel Baccin <daniel.baccin@gmail.com>
2026-03-28 22:56:52 -0300

    test

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,10 @@
 import Fastify from 'fastify'
 
-const app = Fastify({ logger: true })
+const app = Fastify()
 
-app.get('/', async () => {
-  return { ok: true }
+app.get('/', async () => ({ ok: true }))
+
+app.listen({
+  port: 3000,
+  host: '0.0.0.0'
 })
-
-const start = async () => {
-  try {
-    const port = Number(process.env.PORT) || 3000
-
-    await app.listen({
-      port: 3000,
-      host: '0.0.0.0'
-    })
-
-    console.log(`🚀 rodando em 0.0.0.0:${port}`)
-    console.log('ENV PORT:', process.env.PORT)
-    console.log('PORT   :', process.env.PORT)
-
-  } catch (err) {
-    console.error(err)
-    process.exit(1)
-  }
-}
-
-start()
