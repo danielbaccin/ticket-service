31851bc2cb2de03ef1cc1747188208440da39a01
Daniel Baccin <daniel.baccin@gmail.com>
2026-03-28 22:43:15 -0300

    fix: port hardcode

--- src/server.ts
+++ src/server.ts
@@ -11,13 +11,13 @@ const start = async () => {
     const port = Number(process.env.PORT) || 3000
 
     await app.listen({
-      port: port,
+      port: 3000,
       host: '0.0.0.0'
     })
 
     console.log(`🚀 rodando em 0.0.0.0:${port}`)
     console.log('ENV PORT:', process.env.PORT)
-    console.log('PORT RECEBIDA:', process.env.PORT)
+    console.log('PORT   :', process.env.PORT)
 
   } catch (err) {
     console.error(err)
