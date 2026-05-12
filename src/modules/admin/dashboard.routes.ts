af9ffec27e4a0e70cc810abc485b85e55af37f09
Daniel Baccin <daniel.baccin>
2026-05-11 21:04:02 -0300

    fix: id param 2

--- src/modules/admin/dashboard.routes.ts
+++ src/modules/admin/dashboard.routes.ts
@@ -2,5 +2,7 @@ import { FastifyInstance } from 'fastify'
 import { dashboardController } from './dashboard.controller'
 
 export async function dashboardRoutes(app: FastifyInstance) {
-  app.get('/dashboard/:eventId', dashboardController)
+  app.register(dashboardRoutes, {
+    prefix: '/api/admin'
+    })
 }
