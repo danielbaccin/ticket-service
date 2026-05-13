6fc476cd28e7f6aaeef6fdf2960dde003a65ecba
Daniel Baccin <daniel.baccin>
2026-05-12 22:23:53 -0300

    fix: recursive call

--- src/modules/admin/dashboard.routes.ts
+++ src/modules/admin/dashboard.routes.ts
@@ -2,7 +2,5 @@ import { FastifyInstance } from 'fastify'
 import { dashboardController } from './dashboard.controller'
 
 export async function dashboardRoutes(app: FastifyInstance) {
-  app.register(dashboardRoutes, {
-    prefix: '/api/admin'
-    })
+  app.get('/dashboard/:eventId', dashboardController)
 }
