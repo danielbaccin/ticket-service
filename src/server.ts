31257289832bc409f3aa5839efa6d726dbe05802
Daniel Baccin <daniel.baccin>
2026-05-13 21:58:02 -0300

    fix: improves server

--- src/server.ts
+++ src/server.ts
@@ -1,51 +1,85 @@
 import Fastify from 'fastify'
-import { orderRoutes } from './modules/orders/orders.controller'
-import { pool } from './db/pool'
-import { checkinRoutes } from './modules/checkin/checkin.controller'
+import cors from '@fastify/cors'
 import fastifyStatic from '@fastify/static'
 import path from 'path'
+
+import { pool } from './db/pool'
+
+import { orderRoutes, getOrderById } from './modules/orders/orders.controller'
+import { checkinRoutes } from './modules/checkin/checkin.controller'
 import { ticketTypesRoutes } from './modules/ticket-types/ticket-types.controller'
 import { paymentsRoutes } from './modules/payments/payments.controller'
 import { mercadopagoWebhook } from './modules/webhooks/webhooks.controller'
-import { getOrderById } from './modules/orders/orders.controller'
 import { dashboardRoutes } from './modules/admin/dashboard.routes'
-import cors from '@fastify/cors'
 
-const app = Fastify({ logger: true })
+const app = Fastify({
+  logger: {
+    level: 'info'
+  }
+})
 
-// start
+app.setErrorHandler((error, request, reply) => {
+  console.error(error)
+
+  reply.status(500).send({
+    error: 'Internal Server Error'
+  })
+})
+
 const start = async () => {
   try {
     await app.register(cors, {
       origin: true
     })
 
-    // rota raiz (boa prática manter)
     app.get('/', async () => {
       return { ok: true }
     })
 
-    // health com banco
-    app.get('/health', async () => ({ ok: true }))
+    app.get('/health', async () => ({
+      ok: true
+    }))
+
     app.get('/health/db', async () => {
       const result = await pool.query('SELECT 1')
-      return { db: result.rows }
+
+      return {
+        db: result.rows
+      }
     })
 
-    // rotas principais
-    app.register(orderRoutes, { prefix: '/api/orders' })
-    app.register(checkinRoutes, { prefix: '/api/checkin' })
-    app.register(fastifyStatic, {
-      root: path.join(__dirname, '../public'),
+    app.register(orderRoutes, {
+      prefix: '/api/orders'
     })
-    app.register(ticketTypesRoutes, { prefix: '/api/ticket-types' })
-    app.register(paymentsRoutes, { prefix: '/api/payments' })
-    app.post('/api/webhooks/mercadopago', mercadopagoWebhook)
+
     app.get('/api/orders/:id', getOrderById)
+
+    app.register(checkinRoutes, {
+      prefix: '/api/checkin'
+    })
+
+    app.register(ticketTypesRoutes, {
+      prefix: '/api/ticket-types'
+    })
+
+    app.register(paymentsRoutes, {
+      prefix: '/api/payments'
+    })
+
     app.register(dashboardRoutes, {
       prefix: '/api/admin'
     })
 
+    app.post(
+      '/api/webhooks/mercadopago',
+      mercadopagoWebhook
+    )
+
+    app.register(fastifyStatic, {
+      root: path.join(__dirname, '../public')
+    })
+
+    console.log(app.printRoutes())
 
     const port = Number(process.env.PORT) || 3000
 
